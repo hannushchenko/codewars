e103d29c34e74a5b7fc9aedc7596f36fdfe1e0ae
hannushchenko <hannushchenko@gmail.com>
2023-05-08 00:14:05 +0300

    made reversing words in sentence task

--- Fundamentals/01_reverse_words/script.js
+++ Fundamentals/01_reverse_words/script.js
@@ -1,15 +1,26 @@
 function reverseWords(str) {
     // Go for it
-    let arr_str = str.split(" ");
-    let arr_reverse;
-    let reverse_fun = arr_str.forEach(function (str) {
-        let str_reverse;
-        for (l in str) {
-            str_reverse.unshift(l);
+    let array_from_str = str.split(" ");
+    let arr_reverse = [];
+    let str_reverse = [];
+
+
+    array_from_str.forEach(function (str) {
+        for (letter in str) {
+
+            str_reverse.unshift(str[letter]);
+            console.log(letter)
+
+            if (letter == str.length - 1) {
+                arr_reverse.push(str_reverse.join(''))
+                str_reverse = []
+            }
         }
-        str_reverse.push;
-        return str_reverse;
+
+
     })
+
+    return arr_reverse.join(' ');
 }
 
-console.log(reverseWords("hello world"))
+console.log(reverseWords("hello world sasha"))
