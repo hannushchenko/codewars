6f17a3cc2ebfaa268ee71e3a175710f948ef60ea
hannushchenko <hannushchenko@gmail.com>
2023-05-29 23:21:20 +0300

    Made a solution of task to verify guest or boss

---
+++ Fundamentals/29_verify_boss_or_guest/script.js
@@ -0,0 +1,11 @@
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+//Use conditionals to return the proper message:   'Hello boss' or  'Hello guest'
+
+let greet = (n, o) => n === o ? 'Hello boss' : 'Hello guest'
+
+function greet(name, owner) {
+    return Hello`${name == owner ? 'boss' : 'guest'}`
+}
+
+console.log(printerError("Valera", "Valera"));
