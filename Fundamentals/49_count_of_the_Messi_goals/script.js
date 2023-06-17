1c5bfe93cd7809e889512da6e288184e2af2bc48
hannushchenko <hannushchenko@gmail.com>
2023-06-17 20:21:51 +0300

    Made a solution of task to count the number of messi goals

---
+++ Fundamentals/49_count_of_the_Messi_goals/script.js
@@ -0,0 +1,15 @@
+// Messi is a soccer player with goals in three leagues:
+
+//     LaLiga
+//     Copa del Rey
+//     Champions
+
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals
+
+//const goals = (...goalsArray) => goalsArray.reduce((a, b) => a + b);
+
+console.log(goals(5, 4, 2))
