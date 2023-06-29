3d39a0c48ff3a213bdabdbfebd18205a81898f60
hannushchenko <hannushchenko@gmail.com>
2023-06-29 23:26:52 +0300

    Made a solution of task to filter the array to keep digits only

---
+++ Fundamentals/58_filter_the_array_to_keep_only_digits/script.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// Example
+
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+let filter_list = (l) => l.filter(item => typeof item === 'number')
+
+//function filter_list(l) {
+//  return l.filter(e => Number.isInteger(e));
+//}
+
+console.log(filter_list([1, 2, 3, 4, "d"]));
